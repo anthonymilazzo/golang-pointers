1d82c268203af63867adb598a40061b34fbececb
ajmilazzo <ajmilazzo@gmail.com>
2022-10-17 20:20:23 -0400

    printf typos

--- main.go
+++ main.go
@@ -5,22 +5,22 @@ import "fmt"
 // Output:
 // Without pointers:
 // bank_balance1 20
-// bank_balance1 21
+// balance1 21
 // bank_balance1 20
 // ---
 // With pointers:
 // bank_balance2 20
-// bank_balance1 21
+// balance2 21
 // bank_balance2 21
 
 func add_dollar(balance1 int) {
 	balance1 = balance1 + 1
-	fmt.Printf("bank_balance1 %d\n", balance1)
+	fmt.Printf("balance1 %d\n", balance1)
 }
 
 func add_dollar_pointer(balance2 *int) {
 	*balance2 = *balance2 + 1
-	fmt.Printf("bank_balance1 %d\n", *balance2)
+	fmt.Printf("balance2 %d\n", *balance2)
 }
 
 func main() {
